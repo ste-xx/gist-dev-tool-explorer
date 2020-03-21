740b1b57b3306909988bddeed1b1b8a110005b64
ste-xx <stefan.breitenstein@gmail.com>
2020-03-21 13:18:11 +0100

    read all gists, also secret ones

--- src/query.js
+++ src/query.js
@@ -2,7 +2,7 @@ import gql from "graphql-tag";
 
 export const query =  gql(`query {
   viewer {
-    gists (first: 100, orderBy: {field: CREATED_AT, direction: DESC} ) {
+    gists (first: 100, privacy:ALL, orderBy: {field: CREATED_AT, direction: DESC} ) {
       edges {
         node {
           description
